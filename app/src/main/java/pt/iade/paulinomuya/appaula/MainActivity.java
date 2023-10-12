069344aa6263cd8158a4d686bf306780b82c7fff
PaulinoMuya <paulinomuya63@gmail.com>
2023-10-12 11:55:11 +0100

    initial commit

---
+++ app/src/main/java/pt/iade/paulinomuya/appaula/MainActivity.java
@@ -0,0 +1,23 @@
+package pt.iade.paulinomuya.appaula;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // Setup da Activity e do Layout.
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Setup dos Botões.
+
+
+
+        };
+
+    }
